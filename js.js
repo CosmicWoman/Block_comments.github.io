706e93ff83d3645641378e88e12558a37192370a
CosmicWoman <113178584+CosmicWoman@users.noreply.github.com>
2024-02-10 21:33:02 +0300

    Update js.js

--- js.js
+++ js.js
@@ -1,3 +1,28 @@
+let data = `{
+    "comments":[
+    {
+        "name": "Алексей",
+        "text": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim, quisquam!",
+        "date": "2023-03-06T10:00:00.000Z",
+        "is_like": true,
+        "data_counter": "13"
+    },
+    {
+        "name": "Anastasia",
+        "text": "Lorem ipsum dolor sit amet.",
+        "date": "2023-01-02T10:00:00.000Z",
+        "is_like": false,
+        "data_counter": "5"
+    },
+    {
+        "name": "Юлианна",
+        "text": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. A aperiam deleniti dolorem earum excepturi ipsam iusto laboriosam sed velit. Eaque?",
+        "date": "2023-03-07T10:00:00.000Z",
+        "is_like": false,
+        "data_counter": "0"
+    }
+]}`
+
 let jsonData = JSON.parse(data, function (key, value){
    if (key === 'date') {
        return new Date(value)
@@ -222,31 +247,6 @@ set_event_on_keyup('#input_name', errorName);
 set_event_on_keydown('#input_text',  gf);
 
 set_event_on_change('#input_date',  gf);
-
-let data = `{
-    "comments":[
-    {
-        "name": "Алексей",
-        "text": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim, quisquam!",
-        "date": "2023-03-06T10:00:00.000Z",
-        "is_like": true,
-        "data_counter": "13"
-    },
-    {
-        "name": "Anastasia",
-        "text": "Lorem ipsum dolor sit amet.",
-        "date": "2023-01-02T10:00:00.000Z",
-        "is_like": false,
-        "data_counter": "5"
-    },
-    {
-        "name": "Юлианна",
-        "text": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. A aperiam deleniti dolorem earum excepturi ipsam iusto laboriosam sed velit. Eaque?",
-        "date": "2023-03-07T10:00:00.000Z",
-        "is_like": false,
-        "data_counter": "0"
-    }
-]}`
 
 const set_event_on_click = (selector, handler) => {
     let elements = document.querySelectorAll(selector);
