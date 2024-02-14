2172098d769298b68bdff1cab69608baf43a82da
CosmicWoman <113178584+CosmicWoman@users.noreply.github.com>
2024-02-14 10:22:41 +0300

    Create users.js

---
+++ users.js
@@ -0,0 +1,24 @@
+let data = `{
+    "comments":[
+    {
+        "name": "Алексей",
+        "text": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim, quisquam!",
+        "date": "2023-03-06T10:00:00.000Z",
+        "is_like": true,
+        "data_counter": "13"
+    },
+    {
+        "name": "Anastasia",
+        "text": "Lorem ipsum dolor sit amet.",
+        "date": "2023-01-02T10:00:00.000Z",
+        "is_like": false,
+        "data_counter": "5"
+    },
+    {
+        "name": "Юлианна",
+        "text": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. A aperiam deleniti dolorem earum excepturi ipsam iusto laboriosam sed velit. Eaque?",
+        "date": "2023-03-07T10:00:00.000Z",
+        "is_like": false,
+        "data_counter": "0"
+    }
+]}`
